2602b66ff3971ff897eba20d368548d69f8c0c01
JFChoe <jeffrey1855@gmail.com>
2020-08-07 00:29:37 -0400

    adding in build to see what happens

---
+++ build/precache-manifest.8ad5d1663b0a151cbe2faaeb9f1593ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba97ab2b769ffd70cf36fb7362766457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ecf4286ea81f940b1bd",
+    "url": "/static/css/main.67c74981.chunk.css"
+  },
+  {
+    "revision": "79561812c7d9d11a40b9",
+    "url": "/static/js/2.b6bfd007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6bfd007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecf4286ea81f940b1bd",
+    "url": "/static/js/main.79538a68.chunk.js"
+  },
+  {
+    "revision": "be26fa1cc7a1dc70652c",
+    "url": "/static/js/runtime-main.f45dbd0c.js"
+  }
+]);
